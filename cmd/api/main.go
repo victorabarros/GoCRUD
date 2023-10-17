83e2478ad226a4cbe516681015e3533ddb5d18d3
VictorBarros <victorbarros1130@gmail.com>
2023-10-16 21:40:17 -0400

    init implementation of server with gin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	Id        uint   `gorm:"column:id"`
-	FirstName string `gorm:"column:first_name"`
+	Id        uint   `gorm:"column:id"         json:"id"`
+	FirstName string `gorm:"column:first_name" json:"first_name"`
 }
 
 var dsn = "host=localhost user=postgres_user password=postgres_password dbname=postgres_db port=5432 sslmode=disable"
@@ -20,8 +22,23 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	var user User
-	db.Where("id = ?", 2).First(&user)
+	g := gin.Default()
+	g.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
 
-	fmt.Printf("%+v\n", user)
+	g.GET("/", func(c *gin.Context) {
+		var user User
+		db.Where("id = ?", 2).First(&user)
+
+		fmt.Printf("%+v\n", user)
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": user,
+		})
+	})
+
+	g.Run()
 }
