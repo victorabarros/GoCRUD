7eef3ec1c17cbf3675fc73fdf054c3a0134f1a23
VictorBarros <victorbarros1130@gmail.com>
2023-10-16 20:45:52 -0400

    add insert migration

--- database/migrations/1_create_person_table.sql
+++ database/migrations/1_create_person_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS persons (
-    id            SERIAL     PRIMARY KEY,
-    first_name    CHAR(4)    NOT NULL,
-    created_at    TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id         SERIAL     PRIMARY KEY,
+    first_name VARCHAR(12)    NOT NULL,
+    created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
---
+++ database/migrations/2_insert_persons.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons ("first_name")
+VALUES
+    ('John'),
+    ('Garry')
+;
