7de07224966656aff6586412554c1f9da6e05ac9
VictorBarros <victorbarros1130@gmail.com>
2023-10-16 21:29:34 -0400

    add gorm and postgres driver

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	Id        uint   `gorm:"column:id"`
+	FirstName string `gorm:"column:first_name"`
+}
+
+var dsn = "host=localhost user=postgres_user password=postgres_password dbname=postgres_db port=5432 sslmode=disable"
 
 func main() {
-	fmt.Println("hello world")
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var user User
+	db.Where("id = ?", 2).First(&user)
+
+	fmt.Printf("%+v\n", user)
 }
