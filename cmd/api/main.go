866693edebd6e4d27bb2cf8128cf05f15e7ed53e
VictorBarros <victorbarros1130@gmail.com>
2023-10-16 22:28:48 -0400

    add get user controller

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
@@ -10,35 +11,55 @@ import (
 )
 
 type User struct {
-	Id        uint   `gorm:"column:id"         json:"id"`
+	Id        int    `gorm:"column:id"         json:"id"`
 	FirstName string `gorm:"column:first_name" json:"first_name"`
 }
 
 var dsn = "host=localhost user=postgres_user password=postgres_password dbname=postgres_db port=5432 sslmode=disable"
+var db *gorm.DB
 
 func main() {
-	db, err := gorm.Open(postgres.Open(dsn))
+	g := gin.Default()
+
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn))
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	g := gin.Default()
-	g.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
+	SetRoutes(g)
+
+	g.Run()
+}
+
+func SetRoutes(g *gin.Engine) {
+	//TODO move to internal/router
+	g.GET("/user/:id", GetUser)
+
+	g.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+}
 
-	g.GET("/", func(c *gin.Context) {
-		var user User
-		db.Where("id = ?", 2).First(&user)
+func GetUser(ctx *gin.Context) {
+	param := ctx.Param("id")
+	id, err := strconv.Atoi(param)
+	if err != nil {
+		resp := gin.H{"message": fmt.Sprintf("can't parse '%s' to integer", param)}
+		ctx.JSON(http.StatusBadRequest, resp)
+		return
+	}
 
-		fmt.Printf("%+v\n", user)
+	var user User
+	db.Where("id = ?", id).First(&user)
 
-		c.JSON(http.StatusOK, gin.H{
-			"data": user,
-		})
-	})
+	if user.Id != id {
+		resp := gin.H{"message": fmt.Sprintf("user id '%s' not found", param)}
+		ctx.JSON(http.StatusBadRequest, resp)
+		return
+	}
 
-	g.Run()
+	ctx.JSON(http.StatusOK, gin.H{"user": user})
 }
